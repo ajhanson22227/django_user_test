984b2ff570b0e96e5d7ec35a3a99fb8a0fe91e9c
ajhanson22227 <ajhanson22227@gmail.com>
2021-03-10 10:09:39 -0600

    minor edits

--- user_auth_react/src/components/Nav.js
+++ user_auth_react/src/components/Nav.js
@@ -5,16 +5,15 @@ function Nav(props) {
   const logged_out_nav = (
     <ul>
       <li onClick={() => props.display_form("login")}>login</li>
-      <li onClick={() => props.display_form("signup")}>Signup</li>
+      <li onClick={() => props.display_form("signup")}>signup</li>
     </ul>
   );
 
   const logged_in_nav = (
     <ul>
-      <li onClick={() => props.handle_logout}>logout</li>
+      <li onClick={props.handle_logout}>logout</li>
     </ul>
   );
-
   return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
 }
 
